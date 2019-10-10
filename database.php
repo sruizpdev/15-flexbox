cfbe7bf33956f11dd6fd1fe0972fff161a562211
Sergio Ruiz <sergiotto@outlook.com>
2019-10-11 00:07:12 +0200

    primer commit
    Sistema de login

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+
+$server = 'localhost';
+$username = 'sergio';
+$password = 'fidelio';
+$database = 'proyecto';
+
+try {
+  $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
+} catch (PDOException $e) {
+  die('Connection Failed: ' . $e->getMessage());
+}
+
+?>
